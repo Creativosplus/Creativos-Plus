ea0bc238c4186e9b2ba9b6fa1b22bf7ab49b7ac9
Andres Palacios <andrespvzla@gmail.com>
2019-06-07 08:39:09 -0400

    cambios nuevos

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 $(window).scroll(function(){
    if ($(this).scrollTop() > 50) {
-      $('#primary-menu, ul, li').removeClass('menuLinks');
-      $('#primary-menu, ul, li').addClass('menuLinks-alt');
+      $('#primary-menu ul li a').css('color', '#24A35B');
+      $('.colorLink li a').css('color', '#666').hover(function(){
+         $(this).css('color','#24A35B');
+      }, function(){
+         $(this).css('color', '#666');
+      });
    } else {
-      $('#primary-menu, ul, li').removeClass('menuLinks-alt');
-      $('#primary-menu, ul, li').addClass('menuLinks');     
+      $('#primary-menu ul li a').css('color', '#FFF');
+      $('.colorLink li a').css('color', '#666');
    }
 });
